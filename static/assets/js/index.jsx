36bf4b70973fa9aa7d27ee8c26b5efcc99a4d98b
Daniel Shin <shin.daniel.d@gmail.com>
2017-04-04 21:40:24 -0600

    get google maps to display with google-map-react

--- static/assets/js/index.jsx
+++ static/assets/js/index.jsx
@@ -1,13 +1,16 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-import Map from './map.jsx'
+import GoogleMap from './map.jsx'
 
 class Hello extends React.Component {
   render() {
     return (
       <div>
         <h1>Hello, React!</h1>
-        <Map/>
+        <div style={{width: '100%', height: '400px'}}>
+          <GoogleMap/>
+        </div>
+
       </div>
     )
   }
--- static/assets/js/map.jsx
+++ static/assets/js/map.jsx
@@ -1,12 +1,33 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
+import GoogleMapReact from 'google-map-react';
 
-class Map extends React.Component {
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class GoogleMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: {lat: 59.95, lng: 30.33},
+      zoom: 11
+    }
+  }
+
   render() {
-		return (
-			<h1>This is a map</h1>
-		);
-	}
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={{key: 'AIzaSyD0-DcpN0mv2nxOOLUK9PzG0AwjVDcyxZU'}} 
+        defaultCenter={this.state.center}
+        defaultZoom={this.state.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text={'Kreyser Avrora'}
+        />
+      </GoogleMapReact>
+    );
+  }
 }
 
-export default Map;
+export default GoogleMap;
